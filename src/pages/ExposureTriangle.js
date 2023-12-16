c56adc8d88d7e2995d59adfab9f79c5d61c6899f
vividMilan <137170695+vividMilan@users.noreply.github.com>
2023-12-16 19:01:50 +0530

    Debugged

--- src/pages/ExposureTriangle.js
+++ src/pages/ExposureTriangle.js
@@ -5,26 +5,6 @@ import ShutterSpeedSection from "../components/ShutterSpeedSection";
 import IsoSection from "../components/IsoSection";
 
 const ExposureTriangle = () => {
-  const ShutterDetails = [
-    {
-      title: "Low Shutter",
-      imageSrc: "/assets/exposureTriangle/lowShutter.jpg",
-      text: "This is taken with a high shutter speed. Underexposed, we can see all the tower's features but the photograph is too due the underexposure. It would be if it was taken with slower shutter speed.",
-      reverse: false,
-    },
-    {
-      title: "High Shutter",
-      imageSrc: "/assets/exposureTriangle/highShutter.jpg",
-      text: "This is taken with a low shutter speed. over-exposed, the hit the sensor for a too long period of time. Beacuase of this the photograph is over-exposed.",
-      reverse: true,
-    },
-    {
-      title: "Ideal Shutter",
-      imageSrc: "/assets/exposureTriangle/idealShutter.jpg",
-      text: "This is taken with ideal shutter speed. The lotus tower is well defined, but at the same the photograph is not too bright nor dark, Ideally exposed",
-      reverse: false,
-    },
-  ];
 
   return (
     <div className="exposure section">
