8536e2d8e1b1da356d420abea0536c6cb8394ad1
vividMilan <137170695+vividMilan@users.noreply.github.com>
2023-12-16 20:17:49 +0530

    close icon size increased

--- src/components/ShowCase.js
+++ src/components/ShowCase.js
@@ -6,7 +6,7 @@ const ShowCase = ({imageSrc, setShowPic}) => {
   return (
     <div className='showCase'>
           <img src={imageSrc} alt="" />
-          <MdClose onClick={() => setShowPic(false)} className='close-icon' size="1.3em" />
+          <MdClose onClick={() => setShowPic(false)} className='close-icon' size="3rem" />
     </div>
   )
 }
